6094584a9a5298523fd33a326f619f1a198eaf45
Henrique Pyramo <hpyramo92@gmail.com>
2021-03-24 23:28:00 -0300

    Configurações do knex, separação de controllers

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,11 @@
 import express from "express";
+import routes from "./routes";
+import cors from "cors";
 
 const app = express();
 
-app.get("/users", (request, response) => {
-	response.json([{
-		nome: "Henrique"
-	}, {
-		nome: "Daniel"
-	}]);
-});
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
