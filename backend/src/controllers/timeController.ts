176273452bcb35e50285608ea29983161657201f
Henrique Pyramo <hpyramo92@gmail.com>
2021-04-28 20:17:43 -0300

    Configuração ESLint

--- backend/src/controllers/timeController.ts
+++ backend/src/controllers/timeController.ts
@@ -1,27 +1,27 @@
-import {Request, Response} from "express";
+import { Request, Response } from "express";
 import knex from "../database/connection";
 
 class TimeController {
-	async index(request:Request, response: Response) {
-		const times = await knex("time").select("*");
+    async index(request: Request, response: Response) {
+        const times = await knex("time").select("*");
 
-		const timesSerializados = times.map(time => {
-			return {
-				id: time.id,
-				nome: time.nome,
-			}
-		})
+        const timesSerializados = times.map(time => {
+            return {
+                id: time.id,
+                nome: time.nome,
+            };
+        });
 
-		return response.json(timesSerializados);
-	}
+        return response.json(timesSerializados);
+    }
 
-	// show
+    // show
 
-	// create
-	
-	// update
+    // create
 
-	// delete
+    // update
+
+    // delete
 }
 
 export default TimeController;
